713c0684e65ca6531ed9db1e1612e986a3ec23d2
Car Sun <thecarsun@gmail.com>
2026-02-08 08:17:36 -0800

    commit updates

--- src/fetch_news.py
+++ src/fetch_news.py
@@ -8,7 +8,7 @@ import os
 # ---------------------------
 # Paths
 # ---------------------------
-VAULT_ROOT = Path(r"C:\Users\carol\thecarsun\daily-news-obsidian")
+VAULT_ROOT = Path(r"C:\Users\carol\GitHub Repo\daily-news-obsidian")
 TEMPLATE_PATH = VAULT_ROOT / "templates" / "daily-news-template.md"
 OUTPUT_DIR = VAULT_ROOT / "daily"
 
